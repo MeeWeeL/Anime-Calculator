f0238cf22feb91bd0cf22a2170dcf570bbf8da68
MeeWeeL <MeeWeeL@yandex.ru>
2021-05-30 12:41:35 +0300

    start brain

--- app/src/main/java/com/example/hw2/CalculatorBrain.java
+++ app/src/main/java/com/example/hw2/CalculatorBrain.java
@@ -4,122 +4,32 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class CalculatorBrain  extends AppCompatActivity {
+public class CalculatorBrain extends AppCompatActivity {
     String result;
 
-    protected CalculatorBrain() {
+    public CalculatorBrain() {
         if (result == null) {
             result = "0";
         }
-        TextView resultText = findViewById(R.id.calcResult);
-        resultText.setText(result);
     }
-    void setBtn(String btn) {
-        TextView resultText = findViewById(R.id.calcResult);
-        String resultTextView = String.valueOf(resultText);
-        switch (btn) {
+
+    void setBtn(String btnId) {
+        TextView textView = (TextView) findViewById(R.id.calcResult);
+
+        switch (btnId) {
             case "2131230809":
-                btn1(resultText, resultTextView);
-                break;
+                textView.setText("1");
             case "2131230810":
-                btn2(resultText, resultTextView);
-                break;
+                textView.setText("2");
             case "2131230811":
-                btn3(resultText, resultTextView);
-                break;
+                textView.setText("3");
             case "2131230812":
-                btn4(resultText, resultTextView);
-                break;
+                textView.setText("4");
             case "2131230813":
-                btn5(resultText, resultTextView);
-                break;
-            case "2131230814":
-                btn6(resultText, resultTextView);
-                break;
-            case "2131230815":
-                btn7(resultText, resultTextView);
-                break;
-            case "2131230816":
-                btn8(resultText, resultTextView);
-                break;
-            case "2131230817":
-                btn9(resultText, resultTextView);
-                break;
-            case "2131230808":
-                btn0(resultText, resultTextView);
-                break;
+                textView.setText("5");
             default:
-                break;
-        }
-    }
-    private void btn1(TextView resultText, String resultTextView) {
-        if (resultTextView.equals("0")) {
-            resultText.setText("1");
-        } else {
-            resultText.setText(new StringBuilder(resultTextView + "1"));
-        }
-    }
-    private void btn2(TextView resultText, String resultTextView) {
-        if (resultTextView.equals("0")) {
-            resultText.setText("1");
-        } else {
-            resultText.setText(new StringBuilder(resultTextView + "1"));
-        }
-    }
-    private void btn3(TextView resultText, String resultTextView) {
-        if (resultTextView.equals("0")) {
-            resultText.setText("1");
-        } else {
-            resultText.setText(new StringBuilder(resultTextView + "1"));
-        }
-    }
-    private void btn4(TextView resultText, String resultTextView) {
-        if (resultTextView.equals("0")) {
-            resultText.setText("1");
-        } else {
-            resultText.setText(new StringBuilder(resultTextView + "1"));
-        }
-    }
-    private void btn5(TextView resultText, String resultTextView) {
-        if (resultTextView.equals("0")) {
-            resultText.setText("1");
-        } else {
-            resultText.setText(new StringBuilder(resultTextView + "1"));
-        }
-    }
-    private void btn6(TextView resultText, String resultTextView) {
-        if (resultTextView.equals("0")) {
-            resultText.setText("1");
-        } else {
-            resultText.setText(new StringBuilder(resultTextView + "1"));
-        }
-    }
-    private void btn7(TextView resultText, String resultTextView) {
-        if (resultTextView.equals("0")) {
-            resultText.setText("1");
-        } else {
-            resultText.setText(new StringBuilder(resultTextView + "1"));
-        }
-    }
-    private void btn8(TextView resultText, String resultTextView) {
-        if (resultTextView.equals("0")) {
-            resultText.setText("1");
-        } else {
-            resultText.setText(new StringBuilder(resultTextView + "1"));
-        }
-    }
-    private void btn9(TextView resultText, String resultTextView) {
-        if (resultTextView.equals("0")) {
-            resultText.setText("1");
-        } else {
-            resultText.setText(new StringBuilder(resultTextView + "1"));
-        }
-    }
-    private void btn0(TextView resultText, String resultTextView) {
-        if (resultTextView.equals("0")) {
-            resultText.setText("1");
-        } else {
-            resultText.setText(new StringBuilder(resultTextView + "1"));
+                textView.setText(btnId);
         }
     }
 }
+
--- app/src/main/java/com/example/hw2/MainActivity.java
+++ app/src/main/java/com/example/hw2/MainActivity.java
@@ -8,15 +8,18 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
     CalculatorBrain calculator = new CalculatorBrain();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        TextView textView = findViewById(R.id.calcResult);
+        textView.setText(calculator.result);
     }
 
     public void btn(View view) {
-        calculator.setBtn(String.valueOf(view.getId()));
+        String btnId = String.valueOf(view.getId());
+        calculator.setBtn(btnId);
     }
 }
