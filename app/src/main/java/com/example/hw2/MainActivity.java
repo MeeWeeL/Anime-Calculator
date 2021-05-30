b971f0592a32443941a87b8a280c676893333082
MeeWeeL <MeeWeeL@yandex.ru>
2021-05-30 12:02:09 +0300

    start brain

--- app/src/main/java/com/example/hw2/MainActivity.java
+++ app/src/main/java/com/example/hw2/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.hw2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    CalculatorBrain calculator = new CalculatorBrain();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void btn(View view) {
+        calculator.setBtn(String.valueOf(view.getId()));
     }
 }
